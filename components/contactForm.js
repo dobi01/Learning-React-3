96c7e79b7757fe264ebe5844ad75a11c441019b7
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-01-30 18:45:37 +0100

    Fix input

--- components/contactForm.js
+++ components/contactForm.js
@@ -7,11 +7,8 @@ var ContactForm = React.createClass({
     return (
       <form className='contactForm'>
         <input type='text' placeholder='Imię' value={this.props.contact.firstName}>
-        </input>
         <input type='text' placeholder='Nazwisko' value={this.props.contact.lastName}>
-        </input>
         <input type='email' placeholder='Email' value={this.props.contact.email}>
-        </input>
         <button type='submit'>
           Dodaj kontakt
         </button>
